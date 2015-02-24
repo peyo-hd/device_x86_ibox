a865e3babc3c1e7008f3c2af1fe1ab64c72c7396
Peter Yoon <peter.yoon@nemustech.com>
2015-02-24 10:53:02 +0900

    initial commit

---
+++ ibox.mk
@@ -0,0 +1,36 @@
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
+
+PRODUCT_RUNTIMES := runtime_libart_default
+
+PRODUCT_NAME := ibox
+PRODUCT_DEVICE := ibox
+PRODUCT_BRAND := Android
+PRODUCT_MODEL := Android on ibox
+PRODUCT_MANUFACTURER := x86
+
+PRODUCT_AAPT_CONFIG := normal tvdpi hdpi xhdpi
+PRODUCT_AAPT_PREF_CONFIG := xhdpi
+
+include frameworks/native/build/tablet-10in-xhdpi-2048-dalvik-heap.mk
+
+PRODUCT_PACKAGES += \
+    libGLES_android
+
+PRODUCT_COPY_FILES := \
+    frameworks/native/data/etc/android.hardware.ethernet.xml:system/etc/permissions/android.hardware.ethernet.xml \
+    frameworks/native/data/etc/android.hardware.usb.host.xml:system/etc/permissions/android.hardware.usb.host.xml \
+    device/generic/goldfish/camera/media_profiles.xml:system/etc/media_profiles.xml \
+    device/generic/goldfish/camera/media_codecs.xml:system/etc/media_codecs.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml \
+    $(LOCAL_PATH)/ibox_core_hardware.xml:system/etc/permissions/ibox_core_hardware.xml \
+    $(LOCAL_PATH)/init.ibox.rc:root/init.ibox.rc \
+    $(LOCAL_PATH)/init.usb.rc:root/init.usb.rc \
+    $(LOCAL_PATH)/fstab.ibox:root/fstab.ibox \
+    $(PRODUCT_COPY_FILES)
+
+DEVICE_PACKAGE_OVERLAYS := device/x86/ibox/overlay
+PRODUCT_CHARACTERISTICS := tablet,nosdcard
+PRODUCT_LOCALES := en_US,ko_KR,ja_JP,zh_CN
